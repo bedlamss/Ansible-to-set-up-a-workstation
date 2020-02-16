70b8baa0e033c4e9852e8f2645aa71349a6ffcea
bedlamss <l2velial@gmail.com>
2020-02-16 02:22:40 +0200

    change ansible source

--- prepare_system.sh
+++ prepare_system.sh
@@ -19,16 +19,12 @@ prepare_ubuntu() {
 prepare_debian() {
         $SUDO apt update -y
         $SUDO apt dist-upgrade -y
-        $SUDO apt install curl git mc vim facter python3 python3-apt aptitude -y
+        $SUDO apt install software-properties-common curl git mc facter python3 python3-apt aptitude ansible -y
         [ $(uname -m) == "aarch64" ] && $SUDO apt install gcc python3-dev libffi-dev libssl-dev make -y
-
-        PYTHON_BIN=/usr/bin/python
-        install_pip
-        $SUDO pip install python-apt -U
 
         set +x
         echo
-        echo "   Debian Sytem ready for next step."
+        echo "   Ubuntu ready for next step."
         echo
         ansible --version
 }
